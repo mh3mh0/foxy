8b567e440f788d95945e000aeabb434c08812cb2
Your Name <jonxca@icloud.com>
2021-06-13 11:32:10 +0200

    first commit

---
+++ src/lazy.js
@@ -0,0 +1,20 @@
+import { count } from "./utils/manageCount";
+
+const loadImage = (entries) => {
+  entries
+    .filter((entry) => entry.isIntersecting)
+    .forEach((entry) => {
+      console.log(entry);
+      const image = entry.target.firstChild;
+      image.src = image.dataset.src;
+      observer.unobserve(entry.target);
+      count.imageRendered();
+    });
+};
+
+const observer = new IntersectionObserver(loadImage);
+
+export const registerImage = (img) => {
+  observer.observe(img);
+  count.imageLoaded();
+};
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  variants: {
+    transitionProperty: ["responsive", "motion-safe", "motion-reduce"],
+  },
+  theme: {
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
+      roman: "upper-roman",
+    },
+  },
+  extend: {
+    font1: ["Cabin Sketch", cursive],
+  },
+};
